1410a40b0d4a7da051d94eb0a0feb2a2a3db0c8d
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-16 22:48:50 +0300

    Сумма четных

---
+++ SumEven/Program.cs
@@ -0,0 +1,11 @@
+﻿//Sum of even - сумма четных чисел.
+Console.Clear();
+Console.Write("Сумма четных чисел от 1 до 15 равна: ");
+
+int sum = 0;
+for (int num = 2; num < 15; num+=2)
+{
+   sum = sum + num;
+}
+Console.Write($"{sum}");
+
