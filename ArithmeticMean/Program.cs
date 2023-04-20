821c914e31633fbd1d11a1998ded6633bc6f9e44
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-20 22:06:46 +0300

    Среднее аифметическое массива

---
+++ ArithmeticMean/Program.cs
@@ -0,0 +1,12 @@
+﻿//Среднее арефметическое.
+Console.Clear();
+int[] num = {205,195,739,512,995,888,1035,340,739,512,995,888,1035,340};
+int size = 14;
+int sum = 0;
+int ArithmeticMean = 0;
+for (int i = 0; i < size; i++)
+{
+   sum = sum + num[i];
+}
+ArithmeticMean = sum/size;
+Console.WriteLine($"Среднее арефметическое массива равно: {ArithmeticMean}");
