77e684eee4ea064cbbaeeb390c0429edf48bb2e0
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-20 07:51:19 +0300

    Скалярное произведение двух массивов

---
+++ Scalar/Program.cs
@@ -0,0 +1,10 @@
+﻿//Скалярное произведение.
+Console.Clear();
+int[] FirstVector = { 1, 78, 63, 25, 24 };
+int[] CecondVector = { 6, 83, 15, 33, 15 };
+int Scalar = 0;
+for (int i = 0; i < 5; i++)
+{
+   Scalar = Scalar + FirstVector[i] * CecondVector[i];
+}
+Console.WriteLine($"Скалярное произведение: {Scalar}");
