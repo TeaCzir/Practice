68ab24045e8daed3cfc57920fc8ad84c621fff18
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-21 00:35:29 +0300

    Факторила числа: n.

---
+++ Factorial/Program.cs
@@ -0,0 +1,12 @@
+﻿//Факториал числа.
+Console.Clear();
+Console.Write("Введите число: ");
+double N = double.Parse(Console.ReadLine());
+double a = 1;
+double b = 1;
+while (b <= N)
+{
+   a = a * b;
+   b++;
+}
+Console.WriteLine($"Факториал числа: {N} равен числу: {a}");
