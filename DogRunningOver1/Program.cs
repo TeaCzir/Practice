dab003a60a0e4e81e5036ce6d61de92a27fa66d9
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-04-26 01:54:15 +0300

    Макси. Индекс макси. Второе макси. число.

---
+++ DogRunningOver1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Перебегания собаки.
+Console.Clear();
+Console.WriteLine("Сколько раз собака пробежала туда и обратно? ");
+int i = 0;
+int distance = 10000;
+int speedA = 2;
+int speedB = 1;
+int speedC = 5;
+int time = 0;
+bool e = true;
+bool x = false;
+bool course = e||x;
+while (distance > 10)
+{
+   if (e)
+     {
+      time = distance / (speedC + speedA);
+      course = x;
+     }
+     if (x)
+      {
+         time = distance / (speedC + speedB);
+         course = e;
+      }
+   distance = distance - (speedA + speedB) * time;
+   i++;
+}
+Console.WriteLine($"Собака пробежала {i} раз");
---
+++ Second_Max_Number/Program.cs
@@ -0,0 +1,38 @@
+﻿//Второе максимальное число.
+Console.Clear();
+int[] number = { 1, 8, 33, 52, 6, 8, 25, 50 };
+int size = 8;
+int current_index = 0;
+int max_number_index = 0;
+int max = number[0];
+while (current_index < size)
+{
+   if (number[current_index] > max)
+   {
+      max = number[current_index];
+      max_number_index = current_index;
+   }
+   current_index++;
+}
+Console.WriteLine($"Максимальное число равно: {max}");
+Console.WriteLine($"Индекс максимального числа равен: {max_number_index}");
+int cur_index = 0;
+int second_max = number[0];
+if (max_number_index == 0)
+{
+   second_max = number[1];
+}
+while (cur_index < size)
+{
+   if (cur_index != max_number_index)
+   {
+      if (number[cur_index] > second_max)
+      {
+         second_max = number[cur_index];
+      }
+   }
+   cur_index++;
+}
+Console.WriteLine($"Второе максимальное число равно: {second_max}");
+// Console.WriteLine($"Максимальное число равно: {max}");
+// Console.WriteLine($"Индекс максимального числа равен: {max_number_index}");
