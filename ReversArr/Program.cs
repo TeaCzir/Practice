9af97bc6bc6ab27a47fb25e63d7ee870425d5e11
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-05-23 17:58:02 +0300

    Разворот массива в обратном порядке.

---
+++ ReversArr/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Это наш массив: ");
+int[] arr = { 2, 8, 3, 5, 7, 9, 4 };
+for (int i = 0; i < 7; i++)
+{
+   Console.Write($"{arr[i]}, ");
+}
+Console.WriteLine();
+Console.Write("А это массив в обратном порядке: ");
+for (int i = 7 - 1; i >= 0; i--)
+{
+   Console.Write($"{arr[i]}, ");
+}
